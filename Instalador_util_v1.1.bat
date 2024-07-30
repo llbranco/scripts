15a59e4774dfe69609236d0c953b48043ee06edc
Luciano Branco <thelucianobranco@gmail.com>
2024-07-30 12:41:11 -0300

    Avast added, verbose enable

--- Instalador_util_v1.1.bat
+++ Instalador_util_v1.1.bat
@@ -1,15 +1,18 @@
 @echo off
 :: Definindo variaveis do ambiente
-setlocal
+setlocal enabledelayedexpansion
 color 71
-set build=1.0
-set date=29/jul/24
+set build=1.1
+set date=30/jul/24
 set ano=2024
 set versao=Instalador de utilitarios ver: %build% - %date%
 
 rename %~f0 "Instalador_util_v%build%.bat"
 
 title  %versao% -- %ano% -- By: llbranco
+
+:: no caso de erro de login do winget rodar o comando
+:: Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 
 :instalador
 for %%a in (
@@ -42,10 +45,12 @@ Microsoft.VCRedist.2013.x86
 Microsoft.VCRedist.2013.x64
 Microsoft.VCRedist.2015+.x86
 Microsoft.VCRedist.2015+.x64
+:: Avast
+XPDNZJFNCR1B07
 ) do (
 title  %versao% -- Instalando %%a -- By: llbranco
 echo instalando %%a
-winget install -e --id %%a
+winget install -e --id %%a --verbose
 echo.&echo.%echo.
 )
 pause
