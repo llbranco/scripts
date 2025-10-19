fe17ac3eccf678cb4ac1948fefe824def6032a89
Luciano Branco <thelucianobranco@gmail.com>
2025-10-19 00:04:22 -0300

    using ps cmd instead of pure batch

--- update_defender.bat
+++ update_defender.bat
@@ -1,6 +1,10 @@
 @echo off
 echo ver 0.1
 
-set msdefender="%programfiles%\Windows Defender\MpCmdRun.exe"
-%msdefender% -removedefinitions -dynamicsignatures
-%msdefender% -SignatureUpdate
+::force mode
+::set msdefender="%programfiles%\Windows Defender\MpCmdRun.exe"
+::%msdefender% -removedefinitions -dynamicsignatures
+::%msdefender% -SignatureUpdate
+
+:: powershell mode
+powershell.exe -command "& {Update-MpSignature -Verbose; pause}"
