4c62a5468e3678dd43db42c51fd1d3893ad58b8f
Luciano Branco <thelucianobranco@gmail.com>
2025-11-21 16:33:25 -0300

    Update antiupdate_mac.sh to version 1.0.1

--- antiupdate_mac.sh
+++ antiupdate_mac.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 ## by LLBRANCO github.com/llbranco
-echo antiupdate_mac.sh v1.0 21/nov/25
+echo antiupdate_mac.sh v1.0.1 21/nov/25
 
 # bloqueando notificações do MacOS baseado no script:
 # https://gist.github.com/grahampugh/a027206e47a1e4e9f346b884dd8cfdeb
 
 echo Desativando notificações de updates do MacOS
-softwareupdate --ignore macOSInstallerNotification_GM
+sudo softwareupdate --ignore macOSInstallerNotification_GM
+sudo softwareupdate --no-scan
 
 if [[ -d /Library/Bundles/OSXNotification.bundle ]]; then
     echo "OSXNotification.bundle found. Deleting..."
@@ -19,7 +20,7 @@ fi
 # parcialmente baseado em https://www.cyberciti.biz/faq/apple-mac-os-x-update-softwareupdate-bash-shell-command/ mas eu inverti todos os true por false
 echo executando bloqueios de update
 osascript -e "tell application \"System Preferences\" to quit"
-#sudo softwareupdate --no-scan
+
 sudo softwareupdate --schedule off
 # -bool ou -boolean NO/YES ou false/true ? talvez seja necessario incluir .plist
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -boolean false
@@ -37,10 +38,10 @@ sudo defaults write /Library/Preferences/com.apple.commerce.plist AutoUpdate -bo
 softwareupdate --ignore
 
 # resetar softwareupdate
-# sudo softwareupdate --reset-ignored
+sudo softwareupdate --reset-ignored
 
 # para bloquear uma versao especifica use:
-# sudo softwareupdate --ignore "macOS Catalina"
+sudo softwareupdate --ignore "macOS Tahoe"
 
 # bloqueio de via hosts
 # finalizando serviço de dns e limpando cachedns
