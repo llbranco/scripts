7cca0c2c86837da1806482ffdd1894f3708f4610
Luciano Branco <thelucianobranco@gmail.com>
2026-03-10 10:25:36 -0300

    Create remover_senha_mac.sh

---
+++ remover_senha_mac.sh
@@ -0,0 +1,12 @@
+# remover senha mac
+# entre no "single user mode"
+# Intel: Command (⌘) + S
+# ARM: Desliga, segura o power até aparecer opções.
+# selecione Opções > Utilitários > Terminal
+
+echo execute no Single user mode
+pause
+
+# /sbin/mount -uw /
+# rm /var/db/.AppleSetupDone
+# restart
